2fed1f435794066efbaa5cb75172600424650e80
chochy2001 <ohchochy@gmail.com>
2021-03-15 12:36:56 -0500

    Se quito el nombre de la empresa

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Curso Git y GitHub de Capdesis',
+      title: 'Curso Git y GitHub',
       theme: ThemeData(
 
         primarySwatch: Colors.blue,
