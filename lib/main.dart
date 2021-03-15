c46936a3e2624df37073247f13ff951838720ca1
chochy2001 <ohchochy@gmail.com>
2021-03-15 12:41:19 -0500

    Se actualizo el contador

--- lib/main.dart
+++ lib/main.dart
@@ -31,11 +31,11 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 11;
+  int _counter = 10;
 
   void _incrementCounter() {
     setState(() {
-      _counter+=11;
+      _counter++;
     });
   }
 
