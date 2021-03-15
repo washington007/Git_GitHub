23771b09622712d0bc5d25637e19fbd04ff183a1
chochy2001 <54371626+chochy2001@users.noreply.github.com>
2021-03-15 11:50:04 -0500

    Comenzando con lo bueno de Git y GitHub

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
 
-      home: MyHomePage(title: 'GitHub vs GitLab'),
+      home: MyHomePage(title: 'Apenas empieza lo bueno'),
 
     );
   }
