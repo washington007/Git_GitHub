f6261152fdc003b24f1f1298f8a873519c024483
chochy2001 <54371626+chochy2001@users.noreply.github.com>
2021-03-14 20:46:42 -0500

    Se cambio el titulo de la App
    Prueba de cambios en GitHub

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Curso Git',
+      title: 'Curso Git y GitHub',
       theme: ThemeData(
 
         primarySwatch: Colors.blue,
